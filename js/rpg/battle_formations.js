827f7aa78fb17025198756f49490444d4076f884
Dan Dela Rosa <dandelarosadeveloper@gmail.com>
2018-08-19 19:17:33 -0400

    Add more enemies

--- js/rpg/battle_formations.js
+++ js/rpg/battle_formations.js
@@ -19,6 +19,32 @@ function setupBattleFormationForSelectedOption(selectedOption) {
       new PizzaToto(40),
     ];
   }
+  else if (selectedOption === "Fight Formation 4") {
+    enemies = [
+      new Aquarius(42),
+      new Aquarius(42),
+      new Aquarius(42),
+      new Aquarius(42),
+    ];
+  }
+  else if (selectedOption === "Fight Formation 5") {
+    enemies = [
+      new Sagittarius(45),
+      new Sagittarius(45),
+      new Sagittarius(45),
+    ];
+  }
+  else if (selectedOption === "Fight Formation 6") {
+    enemies = [
+      new Gemini(50),
+      new Gemini(50),
+    ];
+  }
+  else if (selectedOption === "Fight Formation 7") {
+    enemies = [
+      new Zankon(60),
+    ];
+  }
   else {
     // Use as a fallback in case the other formations haven't been defined
     enemies = [
